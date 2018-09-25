a3b592a054832c10a0930f209f4a567b17f0067c
vagrant <vagrant@homestead>
2018-09-25 11:22:46 +0000

    add relationships post, category, tag

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function category()
+    {
+        return $this->hasOne(Category::class);
+    }
+
+    public function author()
+    {
+        return $this->hasOne(User::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(
+            Tag::class,
+            'post_tags',
+            'post_id',
+            'tag_id'
+        );
+    }
 }
+
--- app/Tag.php
+++ app/Tag.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    public function posts()
+    {
+        return $this->belongsToMany(
+            Tag::class,
+            'post_tags',
+            'tag_id',
+            'post_id'
+        );
+    }
 }
