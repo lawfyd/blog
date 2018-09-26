e5792c215eeeb7fefc92531090ab2bd1df4a8a07
vagrant <vagrant@homestead>
2018-09-26 10:08:18 +0000

    add Post methods: status, featured

--- app/Post.php
+++ app/Post.php
@@ -65,13 +65,74 @@ class Post extends Model
 
     public function uploadImage($image)
     {
-        if($image == null) {return;}
+        if($image == null) return;
 
         Storage::delete('uploads/' . $this->image);
         $filename = str_random(10) . '.' . $image->extension();
         $image->saveAs('uploads', $filename);
         $this->image = $filename;
         $this->save();
+    }
+
+    public function setCategory($id)
+    {
+        if($id == null) return;
+
+        $this->category_id = $id;
+        $this->save;
+    }
+
+    public function setTags($ids)
+    {
+        if($ids == null) return;
+
+        $this->tags()->sync($ids);
+    }
+
+    public function setDraft()
+    {
+        $this->status = 0;
+        $this->save();
+    }
+
+    public function setPublic()
+    {
+        $this->status = 1;
+        $this->save();
+    }
+
+    public function toggleStatus($value)
+    {
+        if($value == null)
+        {
+            return $this->setDraft();
+        }
+
+        return $this->setPublic();
+
+    }
+
+    public function setFeatured()
+    {
+        $this->is_featured = 1;
+        $this->save();
+    }
+
+    public function setStandart()
+    {
+        $this->is_featured = 0;
+        $this->save();
+    }
+
+    public function toggleFeatured($value)
+    {
+        if($value == null)
+        {
+            return $this->setStandart();
+        }
+
+        return $this->setFeatured();
+
     }
 }
 
