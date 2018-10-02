632019ee705c8478797413af00731a263a2716a5
lawfyd <dimonst93333@gmail.com>
2018-10-02 13:03:50 +0000

    add categories view, controller

---
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CategoriesController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::all();
+        return view('admin.categories.index', ['categories' => $categories]);
+    }
+
+    public function create()
+    {
+        return view('admin.categories.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required'
+        ]);
+        Category::create($request->all());
+        return redirect()->route('categories.index');
+    }
+}
