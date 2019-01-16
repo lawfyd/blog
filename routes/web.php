c4a5f02d46dd37ef12b3068ba9f0a928e3a0ee4f
lawfyd <dimonst93333@gmail.com>
2019-01-16 10:49:52 +0000

    hide for auth and guest

--- routes/web.php
+++ routes/web.php
@@ -14,21 +14,21 @@
     var_dump($query->sql);
 });*/
 
-Route::group([
-   'middleware' => 'admin'
-], function(){
-
-});
-
 Route::get('/', 'HomeController@index');
 Route::get('/post/{slug}', 'HomeController@show')->name('post.show');
 Route::get('/tag/{slug}', 'HomeController@tag')->name('tag.show');
 Route::get('/category/{slug}', 'HomeController@category')->name('category.show');
-Route::get('/register', 'AuthController@registerForm');
-Route::post('/register', 'AuthController@register');
-Route::get('/login', 'AuthController@loginForm');
-Route::post('/login', 'AuthController@login');
-Route::get('/logout', 'AuthController@logout');
+
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('/logout', 'AuthController@logout');
+});
+
+Route::group(['middleware' => 'guest'], function(){
+    Route::get('/register', 'AuthController@registerForm');
+    Route::post('/register', 'AuthController@register');
+    Route::get('/login', 'AuthController@loginForm');
+    Route::post('/login', 'AuthController@login');
+});
 
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'admin'],
     function (){
