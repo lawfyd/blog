0d0c7814a754becf59d41a80f796af78b70324ca
lawfyd <dimonst93333@gmail.com>
2019-01-16 14:24:09 +0000

    add profile

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,14 @@ Route::get('/category/{slug}', 'HomeController@category')->name('category.show')
 
 Route::group(['middleware' => 'auth'], function(){
     Route::get('/logout', 'AuthController@logout');
+    Route::get('/profile', 'ProfileController@index');
+    Route::post('/profile', 'ProfileController@store');
 });
 
 Route::group(['middleware' => 'guest'], function(){
     Route::get('/register', 'AuthController@registerForm');
     Route::post('/register', 'AuthController@register');
-    Route::get('/login', 'AuthController@loginForm');
+    Route::get('/login', 'AuthController@loginForm')->name('login');
     Route::post('/login', 'AuthController@login');
 });
 
