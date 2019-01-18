1827d27a2b69e00e9c64d1d4e5ba54e4237b90ff
lawfyd <dimonst93333@gmail.com>
2019-01-18 15:58:59 +0000

    add author to post

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -32,7 +32,7 @@
 
                             <div class="social-share">
 							<span
-                                    class="social-share-title pull-left text-capitalize">By Rubel On {{ $post->getDate() }}</span>
+                                    class="social-share-title pull-left text-capitalize">By {{ $post->author->name }} On {{ $post->getDate() }}</span>
                                 <ul class="text-center pull-right">
                                     <li><a class="s-facebook" href="#"><i class="fa fa-facebook"></i></a></li>
                                     <li><a class="s-twitter" href="#"><i class="fa fa-twitter"></i></a></li>
@@ -44,11 +44,10 @@
                         </div>
                     </article>
                     <div class="top-comment"><!--top comment-->
-                        <img src="/images/comment.jpg" class="pull-left img-circle" alt="">
-                        <h4>Rubel Miah</h4>
+                        <img src="{{ $post->author->getImage() }}" class="pull-left img-circle" alt="">
+                        <h4>{{ $post->author->name }}</h4>
 
-                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy hello ro mod tempor
-                            invidunt ut labore et dolore magna aliquyam erat.</p>
+                        <p>{{ $post->author->description }}</p>
                     </div><!--top comment end-->
                     <div class="row"><!--blog next previous-->
                         <div class="col-md-6">
