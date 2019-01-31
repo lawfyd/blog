95a9334e52d8f97dfbc742cd60f99cbc12455394
lawfyd <dimonst93333@gmail.com>
2019-01-31 12:34:21 +0000

    fix bug

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -243,7 +243,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <div class="text-center">&copy; 2017
+                    <div class="text-center">&copy; 2019
                     </div>
                 </div>
             </div>
